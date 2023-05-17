0cf285bcb5fcad5b9239a324fc302abb4c23cefa
Darrin Barker <darrin.barker@flatironschool.com>
2023-05-17 18:31:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+// const userData ={
+//     name: "Darrin",
+//     email: "DarrinB@gmail.com",
+// };
+//     const options ={
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//         },
+//         body: JSON.stringify(userData)
+//     };
+
+//     fetch("http://localhost:3000/users", userObj)
+function submitData(userName,userEmail){
+    
+const userData ={
+            name: userName,
+            email: userEmail,
+        };
+
+    const options ={
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify(userData)
+
+
+}
+return fetch("http://localhost:3000/users", options)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp.id)
+        document.querySelector('body').append(resp.id)
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+        document.querySelector('body').append(error.message)
+      });
+    
+
+    
+
+}
